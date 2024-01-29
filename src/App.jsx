1f7db4f03285bbc187222560275c1b3760bb4cdf
Demyrah Bivings <demyrahbivings@Demyrahs-iMac.local>
2024-01-29 13:46:30 -0500

    added some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css'
 import {useState} from 'react'
 import { puppyList } from './data.js'
+import './css.css'
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList: ", puppyList)
-
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
       <div className='App'>
+        <h1>Puppy Pals</h1>
+        <img src="https://pics.craiyon.com/2023-06-02/fb79d3efe4684398899742ec54b95b3c.webp" alt="" />
+        <h3>Click on each puppy to see more!</h3>
         {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })
         }
+        { featPupId && (
+          <div className='Feat'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div> )}
       </div>
-  
   )
 }
 
